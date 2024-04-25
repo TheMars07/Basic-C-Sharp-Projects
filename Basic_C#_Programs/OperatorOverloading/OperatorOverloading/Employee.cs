d8d23288caafb668a4122332b3c082f1a20c1815
TheMars07 <miguelsobral3607@gmail.com>
2024-04-25 13:33:13 +0100

    Operator Assignment

---
+++ Basic_C#_Programs/OperatorOverloading/OperatorOverloading/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading
+{
+    //Define Employee class 
+    public class Employee
+    {
+        //Class properties
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //Constructor
+        public Employee(int id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        //Overloaded == operator
+        public static bool operator== (Employee emp1, Employee emp2)
+        {
+            //Returns true if the Id properties are equal, otherwise returns false
+            return emp1.Id == emp2.Id;
+        }
+
+        //Overloaded != operator
+        public static bool operator!= (Employee emp1, Employee emp2)
+        { 
+            return !(emp1.Id == emp2.Id);
+        }
+    }
+}
